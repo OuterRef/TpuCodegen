cfb722877213a7c71c4a5481110b5d3a969ea1ff
ckq <851879438@qq.com>
2023-04-30 11:17:16 +0800

    to class

--- json_mlir.py
+++ json_mlir.py
@@ -1,6 +1,5 @@
 import json
 import argparse
-from ops import *
 
 class genModel():
     def __init__(self, json_file):
@@ -23,9 +22,7 @@ class genModel():
     
     def genOpCode(self, op):
         opcode = op["opcode"]
-        code, exec_code = self.opcode_table[opcode](op)
-        self.code += code
-        self.exec_code += exec_code
+        self.opcode_table[opcode](op)
 
     def addCode(self, string: str):
         self.code.append(string)
@@ -41,12 +38,71 @@ class genModel():
             for line in self.code:
                 f.write(line)
 
-    @staticmethod
-    def opCodeTable() -> dict:
+    def opCodeTable(self) -> dict:
         ''' opcode : genCodeMethod '''
         return {
-            "tpu.Conv2D" : Conv2D,
+            "tpu.Conv2D" : self.Conv2D,
         }
+    
+    ####################################
+    ####         Operations         ####
+    ####################################
+    def Conv2D(self, op):
+        params = op["attributes"]
+        param_name = "line_" + str(op["file-line"]) + "_param"
+        operands = op["operands"]
+        results = op["results"]
+        self.addCode(f"struct csinn_conv2d_params *{param_name} = csinn_alloc_params(sizeof(struct csinn_conv2d_params), sess);\n")
+        self.addCode(f'{param_name}->stride_height = {params["strides"][0]};\n')
+        self.addCode(f'{param_name}->stride_width = {params["strides"][1]};\n')
+        self.addCode(f'{param_name}->pad_left = {params["pads"][0]};\n')
+        self.addCode(f'{param_name}->pad_right = {params["pads"][1]};\n')
+        self.addCode(f'{param_name}->pad_top = {params["pads"][2]};\n')
+        self.addCode(f'{param_name}->pad_down = {params["pads"][3]};\n')
+        self.addCode(f'{param_name}->dilation_width = {params["dilations"][0]};\n')
+        self.addCode(f'{param_name}->dilation_height = {params["dilations"][1]};\n')
+        self.addCode(f'{param_name}->base.layout = CSINN_LAYOUT_NCHW;\n')
+        self.addCode(f'{param_name}->group = 1;\n')
+        self.addCode(f'{param_name}->conv_extra.fuse_zp2bias = false;\n')
+        self.addCode(f'{param_name}->base.api = CSINN_C906;\n')
+        
+        names = []
+        for idx, value in enumerate(operands+results):
+            name = value["name"]
+            names.append(name)
+            shape = getShapeFromType(value["type"])
+            n_dim = len(shape)
+            self.addCode(f"struct csinn_tensor *{name} = csinn_alloc_tensor(sess);\n")
+            if idx == 1:
+                # weight shape adjustment
+                kernel_size = params["kernel_shape"]
+                self.addCode(f"{name}->dim[0] = {shape[1]};\n")
+                self.addCode(f"{name}->dim[1] = {shape[2]};\n")
+                self.addCode(f"{name}->dim[2] = {kernel_size[0]};\n")
+                self.addCode(f"{name}->dim[3] = {kernel_size[1]};\n")
+            else:
+                for i in range(n_dim):
+                    self.addCode(f"{name}->dim[{i}] = {shape[i]};\n")
+            self.addCode(f"{name}->dim_count = {n_dim};\n")
+            self.addCode(f"{name}->layout = CSINN_LAYOUT_NCHW;\n")
+            self.addCode(f"{name}->is_const = 0;\n")
+            self.addCode(f"{name}->quant_channel = 1;\n")
+            self.addCode(f"{name}->dtype = CSINN_DTYPE_FLOAT32;\n")
+            # TODO: random initialize, replace with real data in the future
+            self.addCode(f"{name}->data = malloc({' * '.join([str(dim) for dim in shape])} * sizeof(float));\n")
+
+        name_series = f"{names[0]}, {names[3]}, {names[1]}, {names[2]}, {param_name}"
+        self.addCode(f'csinn_conv2d_init({name_series});\n')
+        self.exec_code.append(f'csinn_conv2d({name_series});\n')
+
+
+def getShapeFromType(type: str):
+    ''' Get shape array from tensortype '''
+    assert type[:7] == "tensor<"
+    shape_str = type.split("<", 1)[-1].split(",")[0]
+    shape = shape_str.split("x")[:-1]
+    shape = [int(dim) for dim in shape]
+    return shape
 
 def mlirAttr2kwargs(attr_str: str):
     ''' Turn mlir-style attr into kwargs. '''
